04561b97484037f61f37616b2e4bb1335b5e5357
jcabrera53 <juancabrera53@gmail.com>
2022-12-19 01:27:25 +0000

    primera version de About Us

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -11,6 +11,7 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Registro } from "./pages/registro";
+import { AboutUs } from "./pages/about-us";
 
 //create your first component
 const Layout = () => {
@@ -30,6 +31,7 @@ const Layout = () => {
             <Route element={<Single />} path="/single/:theid" />
             <Route element={<h1>Not found!</h1>} />
             <Route element={<Registro />} path="/registro" />
+            <Route element={<AboutUs />} path="/about-us" />
           </Routes>
           <Footer />
         </ScrollToTop>
