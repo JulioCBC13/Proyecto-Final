00b8f91edbc34ad4e2f14de8cb98f780cb0e5b89
jcabrera53 <juancabrera53@gmail.com>
2022-12-25 03:32:10 +0000

    ampliación about us

--- src/front/js/pages/about-us.js
+++ src/front/js/pages/about-us.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../../styles/about-us.css";
+import "../../img/reunion.jpg";
+import "../../img/contador 3.jpg";
 import "../../img/Juan.jpg";
 import "../../img/Julio.jpg";
 import "../../img/Miguel.jpg";
@@ -12,7 +14,20 @@ export const AboutUs = () => {
             <h1>¿Quiénes somos?</h1>
         </div>
         <div className="descripcion">
+        <img src="contador 3.jpg" width="450" height="250" alt="images"/>
+        <div className="texto1">
             <h4>Somos un grupo de desarrolladores que se unió para dar respuesta a una necesidad que se detectó respecto a la gestión de documentos contables y que nos sentíamos capaces de resolver a través de una herramienta moderna, rápida y eficiente. Pasa el mouse sobre las fotos para saber más.</h4>
+            </div>
+        </div>
+        <div className="descripcion2">
+        <div className="texto1">
+            <h4>Somos un grupo de desarrolladores que se unió para dar respuesta a una necesidad que se detectó respecto a la gestión de documentos contables y que nos sentíamos capaces de resolver a través de una herramienta moderna, rápida y eficiente. Pasa el mouse sobre las fotos para saber más.</h4>
+            </div>
+            <img src="reunion.jpg" width="450" height="250" alt="images"/>
+        </div>
+        <br/>
+        <div className="title-page d-flex justify-content-center align-items-center">
+            <h1>Nuestro equipo:</h1>
         </div>
         <br/><br/>
     <div className="box">
