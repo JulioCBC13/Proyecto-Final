cd88afda81f32866ffc8519e036b16c8457e0037
Miguel-web <miguelgilf@gmail.com>
2023-01-08 20:29:05 +0000

    adelanto general home

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,7 +2,33 @@ import React, { Component } from "react";
 import "/workspace/Proyecto-Final/src/front/js/component/navbar.css";
 
 export const Footer = () => (
-  <footer className="footer mt-auto py-3 text-center">
+  /*<footer className="footer mt-auto py-3 text-center">
     <p>© 2022 Copyright: CONTAPLUS+</p>
+  </footer>
+
+/*<!-- Footer-->*/
+  <footer class="bg-dark py-4 mt-auto">
+    <div class="container px-5">
+      <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+        <div class="col-auto">
+          <div class="small m-0 text-white">
+            Copyright &copy; Your Website 2022
+          </div>
+        </div>
+        <div class="col-auto">
+          <a class="link-light small" href="#!">
+            Privacy
+          </a>
+          <span class="text-white mx-1">&middot;</span>
+          <a class="link-light small" href="#!">
+            Terms
+          </a>
+          <span class="text-white mx-1">&middot;</span>
+          <a class="link-light small" href="#!">
+            Contact
+          </a>
+        </div>
+      </div>
+    </div>
   </footer>
 );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -13,6 +13,13 @@ import "../../img/github.png";
 import "../../img/gitpod.png";
 import "../../img/postgre.png";
 import "../../img/sql.png";
+import "../../img/bcv.png";
+import "../../img/btc.png";
+import "../../img/btc.png";
+import "../../img/dolar.png";
+import "../../img/instagram.png";
+import "../../img/facebook.png";
+import "../../img/twitter.png";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -26,24 +33,24 @@ export const Home = () => {
             <div class="row gx-5 align-items-center justify-content-center">
               <div class="col-lg-8 col-xl-7 col-xxl-6">
                 <div class="my-5 text-center text-xl-start">
-                  <h1 class="display-5 fw-bolder text-white mb-2">
-                    A Bootstrap 5 template for modern businesses
+                  <h1 class="display-5 fst-italic fw-bolder text-white mb-2">
+                    Contaplus el Plus que tu Empresa necesita
                   </h1>
                   <p class="lead fw-normal text-white-50 mb-4">
-                    Quickly design and customize responsive mobile-first sites
-                    with Bootstrap, the world’s most popular front-end open
-                    source toolkit!
+                    {/*<!-- TEXTO AQUI!! -->*/}
                   </p>
                   <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
                     <a
                       class="btn btn-primary btn-lg px-4 me-sm-3"
                       href="#features"
                     >
-                      Get Started
+                      Nuestros Servicios
                     </a>
-                    <a class="btn btn-outline-light btn-lg px-4" href="#!">
+                    {/*
+                      <a class="btn btn-outline-light btn-lg px-4" href="#!">
                       Learn More
                     </a>
+                    */}
                   </div>
                 </div>
               </div>
@@ -62,7 +69,7 @@ export const Home = () => {
           <div class="container px-5 my-5">
             <div class="row gx-5">
               <div class="col-lg-4 mb-5 mb-lg-0">
-                <h2 class="fw-bolder mb-0">A better way to start building.</h2>
+                <h2 class="fw-bolder mb-0">SERVICIOS</h2>
               </div>
               <div class="col-lg-8">
                 <div class="row gx-5 row-cols-1 row-cols-md-2">
@@ -70,7 +77,7 @@ export const Home = () => {
                     <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3">
                       <i class="bi bi-collection"></i>
                     </div>
-                    <h2 class="h5">Featured title</h2>
+                    <h2 class="h5">CERTIFICACIÓN DE INGRESOS</h2>
                     <p class="mb-0">
                       Paragraph of text beneath the heading to explain the
                       heading. Here is just a bit more text.
@@ -80,7 +87,7 @@ export const Home = () => {
                     <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3">
                       <i class="bi bi-building"></i>
                     </div>
-                    <h2 class="h5">Featured title</h2>
+                    <h2 class="h5">DECLARACIÓN DE IMPUESTOS</h2>
                     <p class="mb-0">
                       Paragraph of text beneath the heading to explain the
                       heading. Here is just a bit more text.
@@ -90,7 +97,7 @@ export const Home = () => {
                     <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3">
                       <i class="bi bi-toggles2"></i>
                     </div>
-                    <h2 class="h5">Featured title</h2>
+                    <h2 class="h5">BALANCE PERSONAL</h2>
                     <p class="mb-0">
                       Paragraph of text beneath the heading to explain the
                       heading. Here is just a bit more text.
@@ -100,7 +107,7 @@ export const Home = () => {
                     <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3">
                       <i class="bi bi-toggles2"></i>
                     </div>
-                    <h2 class="h5">Featured title</h2>
+                    <h2 class="h5">ASESORÍA FINANCIERA</h2>
                     <p class="mb-0">
                       Paragraph of text beneath the heading to explain the
                       heading. Here is just a bit more text.
@@ -118,22 +125,18 @@ export const Home = () => {
             <div class="row gx-5 justify-content-center">
               <div class="col-lg-8 col-xl-6">
                 <div class="text-center">
-                  <h2 class="fw-bolder">From our blog</h2>
+                  <h2 class="fw-bolder">NOVEDADES FINANCIERAS</h2>
                   <p class="lead fw-normal text-muted mb-5">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Eaque fugit ratione dicta mollitia. Officiis ad.
+                    {/*<!-- TEXTO AQUI!! -->*/}
                   </p>
                 </div>
               </div>
             </div>
             <div class="row gx-5">
               <div class="col-lg-4 mb-5">
+                {/* ---------------- card 1 ---------------- */}
                 <div class="card h-100 shadow border-0">
-                  <img
-                    class="card-img-top"
-                    src="https://dummyimage.com/600x350/ced4da/6c757d"
-                    alt="..."
-                  />
+                  <img class="card-img-top" src="bcv.png" alt="..." />
                   <div class="card-body p-4">
                     <div class="badge bg-primary bg-gradient rounded-pill mb-2">
                       News
@@ -152,11 +155,7 @@ export const Home = () => {
                   <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
                     <div class="d-flex align-items-end justify-content-between">
                       <div class="d-flex align-items-center">
-                        <img
-                          class="rounded-circle me-3"
-                          src="https://dummyimage.com/40x40/ced4da/6c757d"
-                          alt="..."
-                        />
+                        <img class=" me-3" src="twitter.png" alt="..." />
                         <div class="small">
                           <div class="fw-bold">Kelly Rowan</div>
                           <div class="text-muted">
@@ -168,13 +167,10 @@ export const Home = () => {
                   </div>
                 </div>
               </div>
+              {/* ---------------- card 2 ---------------- */}
               <div class="col-lg-4 mb-5">
                 <div class="card h-100 shadow border-0">
-                  <img
-                    class="card-img-top"
-                    src="https://dummyimage.com/600x350/adb5bd/495057"
-                    alt="..."
-                  />
+                  <img class="card-img-top" src="btc.png" alt="..." />
                   <div class="card-body p-4">
                     <div class="badge bg-primary bg-gradient rounded-pill mb-2">
                       Media
@@ -194,11 +190,7 @@ export const Home = () => {
                   <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
                     <div class="d-flex align-items-end justify-content-between">
                       <div class="d-flex align-items-center">
-                        <img
-                          class="rounded-circle me-3"
-                          src="https://dummyimage.com/40x40/ced4da/6c757d"
-                          alt="..."
-                        />
+                        <img class="me-3" src="instagram.png" alt="..." />
                         <div class="small">
                           <div class="fw-bold">Josiah Barclay</div>
                           <div class="text-muted">
@@ -210,13 +202,10 @@ export const Home = () => {
                   </div>
                 </div>
               </div>
+              {/* ---------------- card 3 ---------------- */}
               <div class="col-lg-4 mb-5">
                 <div class="card h-100 shadow border-0">
-                  <img
-                    class="card-img-top"
-                    src="https://dummyimage.com/600x350/6c757d/343a40"
-                    alt="..."
-                  />
+                  <img class="card-img-top" src="dolar.png" alt="..." />
                   <div class="card-body p-4">
                     <div class="badge bg-primary bg-gradient rounded-pill mb-2">
                       News
@@ -238,11 +227,7 @@ export const Home = () => {
                   <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
                     <div class="d-flex align-items-end justify-content-between">
                       <div class="d-flex align-items-center">
-                        <img
-                          class="rounded-circle me-3"
-                          src="https://dummyimage.com/40x40/ced4da/6c757d"
-                          alt="..."
-                        />
+                        <img class=" me-3" src="facebook.png" alt="..." />
                         <div class="small">
                           <div class="fw-bold">Evelyn Martinez</div>
                           <div class="text-muted">
@@ -431,12 +416,8 @@ export const Home = () => {
             <aside class="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
               <div class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
                 <div class="mb-4 mb-xl-0">
-                  <div class="fs-3 fw-bold text-white">
-                    {/*<!-- TITULO AQUI -->*/}
-                  </div>
-                  <div class="text-white-50">
-                    Sign up for our newsletter for the latest updates.
-                  </div>
+                  <div class="fs-3 fw-bold text-white">AGENDAR CITA</div>
+                  <div class="text-white-50">{/*<!-- TEXTO AQUI!! -->*/}</div>
                 </div>
                 <div class="ms-xl-4">
                   <div class="input-group mb-2">
@@ -452,11 +433,11 @@ export const Home = () => {
                       id="button-newsletter"
                       type="button"
                     >
-                      Sign up
+                      Que necesitas?
                     </button>
                   </div>
                   <div class="small text-white-50">
-                    We care about privacy, and will never share your data.
+                    Responderemos todas tus preguntas
                   </div>
                 </div>
               </div>
@@ -464,31 +445,6 @@ export const Home = () => {
           </div>
         </section>
       </main>
-      {/*<!-- Footer-->*/}
-      <footer class="bg-dark py-4 mt-auto">
-        <div class="container px-5">
-          <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-            <div class="col-auto">
-              <div class="small m-0 text-white">
-                Copyright &copy; Your Website 2022
-              </div>
-            </div>
-            <div class="col-auto">
-              <a class="link-light small" href="#!">
-                Privacy
-              </a>
-              <span class="text-white mx-1">&middot;</span>
-              <a class="link-light small" href="#!">
-                Terms
-              </a>
-              <span class="text-white mx-1">&middot;</span>
-              <a class="link-light small" href="#!">
-                Contact
-              </a>
-            </div>
-          </div>
-        </div>
-      </footer>
       {/*<!-- Bootstrap core JS-->*/}
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
       {/*<!-- Core theme JS-->*/}
