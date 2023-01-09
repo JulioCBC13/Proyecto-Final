2462b385163aace7e4d740fef8f79e2407b346cd
BIGOSCAR85 <rojas.osc85@gmail.com>
2023-01-09 23:39:15 +0000

    carousel home mod nav

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -26,6 +26,47 @@ export const Home = () => {
 
   return (
     /*<!--HEADER-->*/
+    /*<!-- INICIO COROUSEL-->*/
+    <div class="carousel">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="contaplus.png" width="200" height="500" class="d-block w-100" alt="..."/>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="bcv.png" width="200" height="500" class="d-block w-100" alt="..."/>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="contador 3.jpg" width="200" height="500" class="d-block w-100" alt="..."/>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    {/*<!-- FIN COROUSEL-->*/}
     <body class="d-flex flex-column h-100">
       <main class="flex-shrink-0">
         <header class="bg-dark py-5">
@@ -243,7 +284,7 @@ export const Home = () => {
             {/*<!-- HERRAMIENTAS/ TECNOLOGIAS/ LENGUAJES USADOS ICONOS-->*/}
             <div class="container px-5 my-5">
               <div class="text-center">
-                <h1 class="fw-bold ">HERRAMIENTAS</h1>
+                <h1 class="fw-bold ">TECNOLOGIAS</h1>
                 <p class="lead fw-normal text-muted mb-3">
                   {/*<!-- TEXTO AQUI!! -->*/}
                 </p>
@@ -450,5 +491,7 @@ export const Home = () => {
       {/*<!-- Core theme JS-->*/}
       <script src="js/scripts.js"></script>
     </body>
+    {/*<!-- DIV FINAL COROUSEL-->*/}
+    </div>
   );
 };
