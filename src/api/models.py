7d3966dbe1cb8566af8d85dfcbbbfc065593943b
JulioCBC13 <blandojulio@gmail.com>
2023-01-11 23:18:45 +0000

    cambiando base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    lastName = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True,nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    cedula = db.Column(db.String(100), unique=True, nullable=False)
 
+    
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.username
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+    def set_password(self,password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password, password)
--- src/crud/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    # clase de configuracion de phyton
-    SECRET_KEY = "contadores secretos"
-    SQLALCHEMY_DATABASE_URI = "/workspace/Proyecto-Final/contaplus.db"
-    SQL_TRACK_MODIFICATIONS = False
-    
