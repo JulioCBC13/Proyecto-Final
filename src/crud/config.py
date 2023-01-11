019fffe78ce7cf5e7af31e92250f16bb0d224dbf
JulioCBC13 <blandojulio@gmail.com>
2023-01-11 18:57:43 +0000

    creacion de base de datos

---
+++ src/crud/config.py
@@ -0,0 +1,6 @@
+class Config:
+    # clase de configuracion de phyton
+    SECRET_KEY = "contadores secretos"
+    SQLALCHEMY_DATABASE_URI = "/workspace/Proyecto-Final/contaplus.db"
+    SQL_TRACK_MODIFICATIONS = False
+    
