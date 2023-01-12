549f514e65f8479ebad783560114b6686a40cbd4
JulioCBC13 <blandojulio@gmail.com>
2023-01-12 21:17:32 +0000

    modificacion de user a usuario y creacion de clase contadores

--- src/api/models.py
+++ src/api/models.py
@@ -4,8 +4,8 @@ from werkzeug.security import generate_password_hash,check_password_hash
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'users'
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -19,6 +19,31 @@ class User(db.Model):
     
     def __repr__(self):
         return '<User %r>' % self.username
+
+    def set_password(self,password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password, password)
+
+
+class Contador(db.Model):
+    __tablename__ = 'contadores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    lastName = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True,nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    cedula = db.Column(db.String(100), unique=True, nullable=False)
+    cpc = db.Column(db.String(8), unique=True, nullable=False)
+    
+
+    
+    def __repr__(self):
+        return '<Contador %r>' % self.Contador
 
     def set_password(self,password):
         self.password = generate_password_hash(password)
